350d94d262c8fbfa628102cd8fe21aaf8a13f4db
Sebastian <sebastian.eder@htlpinkafeld.at>
2025-09-06 11:20:31 +0200

    added new function

--- app.js
+++ app.js
@@ -6,3 +6,8 @@ function User(name, age) {
 User.prototype.sayHello = function() {
     console.log("Hello, my name is " + this.name + " and I am " + this.age + " years old.");
 };
+
+User.prototype.haveBirthday = function() {
+    this.age += 1;
+    console.log("Happy birthday " + this.name + "! You are now " + this.age + " years old.");
+};
